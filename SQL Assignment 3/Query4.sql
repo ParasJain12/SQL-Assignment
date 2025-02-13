2883157a124de2a3d3b07598a48d1642077f0b97
Paras Jain <119165520+ParasJain12@users.noreply.github.com>
2025-02-13 15:56:33 +0530

    Update Query4.sql

--- SQL Assignment 3/Query4.sql
+++ SQL Assignment 3/Query4.sql
@@ -10,4 +10,10 @@
 -- APPEASEMENTS $ TOTAL
 
 Solution:-
-
+select count(ri.return_id) as TotalReturn,
+       sum(ri.return_price * return_quantity) as Return_Total,
+       count(ra.return_id) as TotalAppeasement,
+       sum(ra.amount) as Appeasement_Total
+from Return_item ri 
+JOIN return_adjustment ra on ra.return_id = ri.return_id
+where ra.return_adjustment_type_id = 'APPEASEMENT';
